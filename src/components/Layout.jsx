7243cf349bbc9434b8e4a4e2fab4562855b71fda
irobinda01 <irobindachinonso@gmail.com>
2024-07-09 16:59:23 +0100

    commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Link, NavLink, Outlet } from "react-router-dom"
+import "./Layout.css"
 
 const Layout = () => {
     return(
         <div>
-            <Link to="/"><h1>#VANLIFE</h1></Link>
             <nav>
-                <NavLink to="host">Host</NavLink>
-                <NavLink to="about">About</NavLink>
-                <NavLink to="vans">Vans</NavLink>
+                <Link to="/"><h1>#VANLIFE</h1></Link>
+                <div className="links">
+                    <NavLink to="host">Host</NavLink>
+                    <NavLink to="about">About</NavLink>
+                    <NavLink to="vans">Vans</NavLink>
+                </div>
             </nav>
             <Outlet/>
         </div>
