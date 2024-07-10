2536c61c70c071f69dd8430fbddf1c892a16cc4a
irobinda01 <irobindachinonso@gmail.com>
2024-07-10 13:49:54 +0100

    commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, NavLink, Outlet } from "react-router-dom"
 import "./Layout.css"
 
 const Layout = () => {
+    const [burger_class, sertBurgerClass] = useState("burger-bar unclicked")
+    const [menu_class, setMenuClass] = useState("menu hidden")
+    const [isMenuClicked, setIsMenuClicked] = useState(false)
+
+    const updateMenu = ()=>{
+        if(!isMenuClicked){
+            sertBurgerClass("burger-bar clicked")
+            setMenuClass("menu vissable")
+        }
+        else{
+            sertBurgerClass("burger-bar unclicked")
+            setMenuClass("menu hidden")
+        }
+        setIsMenuClicked(!isMenuClicked)
+    }
+
     return(
         <div>
             <nav>
                 <Link to="/"><h1>#VANLIFE</h1></Link>
-                <div className="links">
+                <div className="burger-menu" onClick={updateMenu}>
+                    <div className={burger_class}></div>
+                    <div className={burger_class}></div>
+                    <div className={burger_class}></div>
+                </div>
+                <div className={menu_class}>
                     <NavLink to="host">Host</NavLink>
                     <NavLink to="about">About</NavLink>
                     <NavLink to="vans">Vans</NavLink>
