92fd66735db2c350c42e586952b18ab0bb07b462
irobinda01 <irobindachinonso@gmail.com>
2024-07-11 18:42:29 +0100

    commit

--- src/page/About.jsx
+++ src/page/About.jsx
@@ -1,11 +1,24 @@
 import React from "react";
+import "./About.css";
 
-const About = ()=> {
-    return (
-        <div>
-            <h1>The about page</h1>
-        </div>
-    )
-}
+const About = () => {
+  return (
+    <div className="about-box">
+      <div className="about-image"></div>
+      <div className="about-contanier">
+        <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
+        <p className="about-p1">
+          Our mission is to enliven your road trip with the perfect travel van
+          rental. Our vans are recertified before each trip to ensure your
+          travel plans can go off without a hitch. (Hitch costs extra 😉)
+        </p>
+        <p>
+          Our team is full of vanlife enthusiasts who know firsthand the magic
+          of touring the world on 4 wheels.
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default About;
