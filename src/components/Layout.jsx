88b7759b8e6bba23c5e0660b26309dfec2edbcd8
irobinda01 <irobindachinonso@gmail.com>
2024-07-11 15:25:42 +0100

    commit

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,18 +5,21 @@ import "./Layout.css"
 const Layout = () => {
     const [burger_class, sertBurgerClass] = useState("burger-bar unclicked")
     const [menu_class, setMenuClass] = useState("menu hidden")
-    const [isMenuClicked, setIsMenuClicked] = useState(false)
+    const [isMenuUnclicked, setIsMenuUnclicked] = useState(true)
+    const [outlet_container, setOutletContainer] = useState("outlet-container")
 
     const updateMenu = ()=>{
-        if(!isMenuClicked){
+        if(isMenuUnclicked){
             sertBurgerClass("burger-bar clicked")
-            setMenuClass("menu vissable")
+            setMenuClass("menu visible")
+            setOutletContainer("outlet-container outlet-down")
         }
         else{
             sertBurgerClass("burger-bar unclicked")
-            setMenuClass("menu hidden")
+            setMenuClass("menu")
+            setOutletContainer("outlet-container outlet-up")
         }
-        setIsMenuClicked(!isMenuClicked)
+        setIsMenuUnclicked(!isMenuUnclicked)
     }
 
     return(
@@ -28,13 +31,15 @@ const Layout = () => {
                     <div className={burger_class}></div>
                     <div className={burger_class}></div>
                 </div>
-                <div className={menu_class}>
-                    <NavLink to="host">Host</NavLink>
-                    <NavLink to="about">About</NavLink>
-                    <NavLink to="vans">Vans</NavLink>
-                </div>
             </nav>
-            <Outlet/>
+            <div className={menu_class}>
+                <NavLink to="host">Host</NavLink>
+                <NavLink to="about">About</NavLink>
+                <NavLink to="vans">Vans</NavLink>
+            </div>
+            <main className={outlet_container}>
+                <Outlet/>
+            </main>
         </div>
     )
 }
