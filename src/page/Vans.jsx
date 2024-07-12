bbb10dc6539a9804b39fa14c8a4860729dbd5fed
irobinda01 <irobindachinonso@gmail.com>
2024-07-12 17:33:17 +0100

    commit

--- src/page/Vans.jsx
+++ src/page/Vans.jsx
@@ -1,46 +1,56 @@
 import React, { useState, useEffect } from "react";
-import "./Vans.css"
+import { Link } from "react-router-dom"
+import "./Vans.css";
 
-const Vans = ()=> {
-    const [vansData, setVansData] = useState(null);
-    const [state, setState] = useState(false);
+const Vans = () => {
+  const [vansData, setVansData] = useState(null);
+  const [state, setState] = useState(false);
 
-    const getData = async ()=> {
-        const response = await fetch("/api/vans");
-        const data = await response.json();
-        setVansData(data.vans);
-        setState(true);
-        console.log(data.vans);
-    }
+  const getData = async () => {
+    const response = await fetch("/api/vans");
+    const data = await response.json();
+    setVansData(data.vans);
+    setState(true);
+    console.log(data.vans);
+  };
 
-    useEffect(()=>{
-        getData();
-    }, [])
+  useEffect(() => {
+    getData();
+  }, []);
 
-    return (
-        <div className="vans-box">
-            <h1>Explore our van options</h1>
-            <div style={{width: "100%"}} className="van-container">
-                {
-                    vansData ? (
-                        vansData.map((van)=> {
-                            return(
-                                <div key={van.id} className="van-box">
-                                    <img src={van.imageUrl}/>
-                                    <div>
-                                        <h3>{van.name}</h3>
-                                        <h3>${van.price}</h3>
-                                    </div>
-                                    <p>/day</p>
-                                    <button>{van.type}</button> 
-                                </div>
-                            )
-                        })
-                    ) : <h1>Loading...</h1>
-                }
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="vans-box">
+      <h1>Explore our van options</h1>
+      <div style={{ width: "100%" }} className="van-container">
+        {vansData ? (
+          vansData.map((van) => {
+            let backColor =
+              van.type === "simple"
+                ? "#E17654"
+                : van.type === "rugged"
+                ? "#115E59"
+                : "#161616";
+
+            return (
+                <div key={van.id} className="van-box">
+                    <img src={van.imageUrl} />
+                    <div className="vans-text">
+                    <h3>{van.name}</h3>
+                    <h3>${van.price}</h3>
+                    </div>
+                    <p className="vans-fix">/day</p>
+                    <button style={{ backgroundColor: backColor }}>
+                    {van.type}
+                    </button>
+                </div>
+            );
+          })
+        ) : (
+          <h1>Loading...</h1>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default Vans;
