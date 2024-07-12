0dad4a2c0358355f8b75b330b6c4d41c45bf6b80
irobinda01 <irobindachinonso@gmail.com>
2024-07-12 15:56:14 +0100

    commit

--- src/page/Vans.jsx
+++ src/page/Vans.jsx
@@ -1,20 +1,44 @@
 import React, { useState, useEffect } from "react";
+import "./Vans.css"
 
 const Vans = ()=> {
-    const [vansData, setVansData] = useState(null)
+    const [vansData, setVansData] = useState(null);
+    const [state, setState] = useState(false);
 
     const getData = async ()=> {
         const response = await fetch("/api/vans");
         const data = await response.json();
+        setVansData(data.vans);
+        setState(true);
         console.log(data.vans);
     }
 
-    getData();
-
+    useEffect(()=>{
+        getData();
+    }, [])
 
     return (
         <div className="vans-box">
-            <h1>The vans page</h1>
+            <h1>Explore our van options</h1>
+            <div style={{width: "100%"}} className="van-container">
+                {
+                    vansData ? (
+                        vansData.map((van)=> {
+                            return(
+                                <div key={van.id} className="van-box">
+                                    <img src={van.imageUrl}/>
+                                    <div>
+                                        <h3>{van.name}</h3>
+                                        <h3>${van.price}</h3>
+                                    </div>
+                                    <p>/day</p>
+                                    <button>{van.type}</button> 
+                                </div>
+                            )
+                        })
+                    ) : <h1>Loading...</h1>
+                }
+            </div>
         </div>
     )
 }
